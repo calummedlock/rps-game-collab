f877798afb4a2801d62f8448a22473a2959805e8
Calum Medlock <calum.medlock@calmandesign.co.uk>
2023-01-05 16:33:01 +0000

    added more code to js

--- calum-script.js
+++ calum-script.js
@@ -1,4 +1,32 @@
 function playGame(userChoice) {
   let choose = ["r","p","s"]
   let computerChoice = choose[Math.floor(Math.random() * choose.length)];
+
+  
+
+}
+
+  // let userChoice = prompt("Enter your choice (type r, s, or p)");
+  // playGame(userChoice)
+
+  let wins = 0;
+  let ties = 0;
+  let losses = 0;
+
+  for (let index = 0; index < 10; index++) {
+    let userChoice = prompt("Enter your choice (type r, s, or p)");
+
+    if (userChoice == computerChoice) {
+      console.log ("It's a tie!");
+    } else if (userChoice == "r" && computerChoice == "s") {
+      console.log ("You win!");
+    } else if (userChoice == "p" && computerChoice == "r") {
+      console.log ("You win!");
+    } else if (userChoice == "s" && computerChoice == "p") {
+      console.log ("You win!");
+    } else {
+      console.log ("You lose!")
+    }
+
+    if (userChoice == computerChoice)
 }
